83cccc51d997a5a4087f85caa0ba497770cfaab9
nwilkinson3 <wilkin40@go.stockton.edu>
2022-10-03 15:18:44 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,8 +4,9 @@ public class Hello {
 		 
 		 System.out.println("Hello CSCI 4485, " + argv[0] + "!");
 		 
-		 // Manny did modify this, as you can see, I needed to reinstall Eclipse
-		 // on my laptop and I changed this to make sure it worked.
+		 // Manny did modify this, as you can see in the history.
+		 // I needed to reinstall Eclipse on my laptop and I changed
+		 // this to make sure it worked.
 		 
 	 }
 	
