a4d943cfef6bbc36398b4233f5a5be71d1d93524
nwilkinson3 <wilkin40@go.stockton.edu>
2022-10-03 15:17:55 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 	
 	 public static void main( String argv[] ) {
 		 
-		 System.out.println( "Hello, funny-face!" );
+		 System.out.println("Hello CSCI 4485, " + argv[0] + "!");
 		 
 		 // Manny did modify this, as you can see, I needed to reinstall Eclipse
 		 // on my laptop and I changed this to make sure it worked.
