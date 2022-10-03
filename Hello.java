81a637457d010a942c95978c7e08fd3043b4cd8d
nwilkinson3 <wilkin40@go.stockton.edu>
2022-10-03 15:16:58 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 		 
 		 System.out.println( "Hello, funny-face!" );
 		 
+		 // Manny did modify this, as you can see, I needed to reinstall Eclipse
+		 // on my laptop and I changed this to make sure it worked.
+		 
 	 }
 	
 }
